a864386d004df130a6916073851bb8ce98df6923
PretzelCompact <76824671+PretzelCompact@users.noreply.github.com>
2022-03-06 18:49:09 +0400

    Update Task4.java

--- stage1/part4/Task4.java
+++ stage1/part4/Task4.java
@@ -6,28 +6,23 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Task4 {
-    private static int n;
-    private static int m;
-
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите n (число повторений):");
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         System.out.println("Введите m (ограничение по количеству слов)");
-        m = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
         System.out.println("Введите строку:");
-        var sb = new StringBuilder();
-        Arrays.stream(br.readLine().split(" ")).toList().forEach(word->{
-            word += ", ";
-            processWord(sb,word);
-        });
-
-        System.out.println(sb.toString());
-    }
-    private static void processWord(StringBuilder sb, String word){
-        for(int i = 0; i < n && m>0; i++, m--){
-            sb.append(word);
-        }
+        Arrays.stream(br.readLine().split(" "))
+                .flatMap(w -> {
+                    w += ", ";
+                    var arr = new String[n];
+                    for(int i = 0; i < n; i++)
+                        arr[i] = w;
+                    return Arrays.stream(arr);
+                })
+                .limit(m)
+                .forEach(System.out::print);
     }
 }
